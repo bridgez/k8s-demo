ac2d9ce33a045fedc4cd4114935d788cb96f26ba
bridgez <efool.qq.com>
2020-04-01 16:38:31 +0800

    rht

---
+++ rht.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+NODE=$1
+cat <<eof
+####################
+1. backup
+2. reset
+3. quit
+####################
+eof
+read -p "please enter [1|2|3]:" re
+case $re in
+1|backup)   # 这里可以同时判断输入的是1还是backup , 区别于if 判断
+        echo "backup $NODE"
+        sudo virsh dumpxml $NODE |awk -F"'" '/source file/{print $(NF-1)}'
+				IMG=`sudo virsh dumpxml $NODE |awk -F"'" '/source file/{print $(NF-1)}'`
+				OVL=/mnt/wd/images/$NODE.ovl
+				sudo virsh destroy $NODE
+				sudo qemu-img create -q -f qcow2 -b $IMG $OVL
+				sudo sed -i.bak "s#$IMG#$OVL#g" /etc/libvirt/qemu/$NODE.xml
+				sudo virsh define /etc/libvirt/qemu/$NODE.xml
+				;;
+2|reset)
+        echo "reset";;
+3|quit)
+        echo "quit" && exit
+        ;;
+*)
+        echo "attention your input!!!"
+        echo "USAGE: $0 {1|2|3}"
+esac
